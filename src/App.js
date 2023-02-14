c2472677e7d667a1535edb8767b924955666d634
“Brenno <brennoeudes@gmail.com>
2023-02-14 11:45:27 -0300

    Criação card elementos Home

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import NavBar from "./Components/NavBar";
 import { Footer } from "./Components/footer";
+import Card from "./Components/Card";
 
 function App() {
   return (
     <div className="App">
       <NavBar />
+      <Card />
       <Footer />
     </div>
   );
--- src/Pages/Home/index.js
+++ src/Pages/Home/index.js
@@ -1,9 +1,11 @@
 import NavBar from "../../Components/NavBar";
+import Card from "../../Components/Card";
 
 function Home() {
   return (
     <>
       <NavBar />
+      <Card />
     </>
   );
 }
